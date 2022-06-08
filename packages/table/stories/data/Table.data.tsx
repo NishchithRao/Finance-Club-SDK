8b93986277e9cabe93aa529eb80255722d591ea4
NishchithRao <nishchitrao5@gmail.com>
2022-06-09 02:03:28 +0530

    feat(table): add sample table data for storybook

---
+++ packages/table/stories/data/Table.data.tsx
@@ -0,0 +1,48 @@
+export const colorData = [
+  {
+    color: "red",
+    value: "#f00",
+  },
+  {
+    color: "green",
+    value: "#0f0",
+  },
+  {
+    color: "blue",
+    value: "#00f",
+  },
+  {
+    color: "cyan",
+    value: "#0ff",
+  },
+  {
+    color: "magenta",
+    value: "#f0f",
+  },
+  {
+    color: "yellow",
+    value: "#ff0",
+  },
+  {
+    color: "black",
+    value: "#000",
+  },
+];
+
+export const userData = [
+  {
+    firstName: "john",
+    lastName: "doe",
+    age: 31,
+  },
+  {
+    firstName: "alex",
+    lastName: "thompson",
+    age: 41,
+  },
+  {
+    firstName: "tom",
+    lastName: "holland",
+    age: 51,
+  },
+];
