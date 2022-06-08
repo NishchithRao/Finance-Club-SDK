790ce20f6c4c6889b0228b8a88284e944c6f31ef
NishchithRao <nishchitrao5@gmail.com>
2022-06-09 02:03:52 +0530

    feat(table): create table stories

---
+++ packages/table/stories/Table.stories.tsx
@@ -0,0 +1,17 @@
+import { Table } from "../src";
+import { TableType } from "../src/Table";
+import { colorData, userData } from "./data/Table.data";
+
+export default {
+  component: Table,
+  title: "components/table",
+};
+
+export const basic = (args: TableType<any>) => <Table {...args} />;
+basic.args = {
+  data: userData,
+} as TableType<any>;
+
+export const colors = (args: TableType<{ [key: string]: string }>) => (
+  <Table data={colorData} />
+);
