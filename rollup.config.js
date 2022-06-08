62f2a47d24188a6f8187703491c65125a7cd341e
NishchithRao <nishchitrao5@gmail.com>
2022-06-09 02:54:12 +0530

    chore(build): add scss plugin to root config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
+import scss from "rollup-plugin-scss";
+import { uglify } from "rollup-plugin-uglify";
 
 export default {
   input: "./packages/index.ts",
@@ -9,12 +11,16 @@ export default {
     format: "cjs",
   },
   plugins: [
+    uglify(),
     resolve(),
     commonjs(),
     typescript({
       tsconfig: "./tsconfig.json",
       declaration: true,
       declarationDir: "dist",
+    }),
+    scss({
+      output: "dist/table.css",
     }),
   ],
   external: ["react", "react-dom"],
